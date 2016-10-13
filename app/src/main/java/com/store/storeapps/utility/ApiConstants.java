5003bb991864f891bd8aa538904c52dc300de6fd
Baburao4790 <Cherry!@#$>
2016-10-13 08:34:33 +0530

    changes for login by Babu Rao

--- app/src/main/java/com/store/storeapps/utility/ApiConstants.java
+++ app/src/main/java/com/store/storeapps/utility/ApiConstants.java
@@ -24,20 +24,12 @@ public class ApiConstants {
     public static final String CAPTURE_USER_APP_VERSION = BASE_URL + "CaptureUserAppVersion.php";
     public static final String INSERT_PREVIOUS_PRODUCTS = BASE_URL + "insertpreviousproducts.php";
     public static final String CONTACT_US = BASE_URL + "contactus.php";
-
-
-
-
-
-
-
-
-
     public static final String MY_ORDERS = BASE_URL + "my_orders.php";
-
+    public static final String CHECKOUT_ADDRESS = BASE_URL + "checkout_address.php";
+    public static final String PROMO_CHECK = BASE_URL + "promocheck.php";
+    public static final String DELETE_PROMOCODE = BASE_URL + "delete_promocode.php";
     public static final String FORMS_SUBMIT = URL_FORMS + "FormSubmit.php";
     public static final String CANCEL_RESONS = URL_FORMS + "get_CancelReasons.php";
-
     public static final String RETURN_REASONS =URL_FORMS +"get_returnreasons.php";
     public static final String RETURN_TYPE =URL_FORMS +"get_returntype.php";
 }
