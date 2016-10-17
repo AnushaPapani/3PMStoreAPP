97f8747a4b91c200ae167b7bd53c38f44ca3509c
Baburao4790 <Cherry!@#$>
2016-10-18 00:59:14 +0530

    changes for login by Babu Rao

--- app/src/main/java/com/store/storeapps/activities/GCMApplicationConstants.java
+++ app/src/main/java/com/store/storeapps/activities/GCMApplicationConstants.java
@@ -2,7 +2,7 @@ package com.store.storeapps.activities;
 
 public class GCMApplicationConstants {
 	// Php Application URL to store Reg ID created
-	public static final String APP_SERVER_URL = "http://www.3pmstore.com/android/android_connect/insertuser.php";
+	public static final String APP_SERVER_URL = "http://8daysaweek.in/3productsaday/3PMstoreApp/3PMstore5189062/insertuser.php";
 
 
 	public static final String GOOGLE_PROJ_ID = "396260697037";
